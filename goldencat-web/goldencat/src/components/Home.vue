37ca59a9c1a25ecff32bea158a8c2808f4847a58
kingDuiDui <m18508218948@163.com>
2018-06-09 23:53:31 +0800

    web update

---
+++ goldencat-web/goldencat/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>{{msg}}</h3>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: '欢迎来到慕和兰道304的记账系统'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3{
+    text-align: center;
+  }
+</style>
