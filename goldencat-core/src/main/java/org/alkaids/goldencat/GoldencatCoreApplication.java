2fab323c96d36aca45cf4777669f6e26018d4bab
ZhangHaixin <jordan.hank@hotmail.com>
2018-06-09 22:48:24 +0800

    初始化项目结构

---
+++ goldencat-core/src/main/java/org/alkaids/goldencat/GoldencatCoreApplication.java
@@ -0,0 +1,12 @@
+package org.alkaids.goldencat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldencatCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldencatCoreApplication.class, args);
+	}
+}
